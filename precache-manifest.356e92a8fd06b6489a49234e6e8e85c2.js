4205ad59b9c4643c5143c548a0b768c58d3c5413
kumarvaibhav45 <kumarvaibhav45@github.com>
2019-12-28 12:53:47 +0530

    Updates

---
+++ precache-manifest.356e92a8fd06b6489a49234e6e8e85c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de476cf6b1c6811fa00c75d9788ad51",
+    "url": "/Pomodoro-clock/index.html"
+  },
+  {
+    "revision": "a2b428992a2cbb2b66e0",
+    "url": "/Pomodoro-clock/static/css/main.ffecb42d.chunk.css"
+  },
+  {
+    "revision": "937ab973fcb9d2f77bfe",
+    "url": "/Pomodoro-clock/static/js/2.1ead49b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Pomodoro-clock/static/js/2.1ead49b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2b428992a2cbb2b66e0",
+    "url": "/Pomodoro-clock/static/js/main.4caa9846.chunk.js"
+  },
+  {
+    "revision": "a17cf8f5a50d6ed8ff70",
+    "url": "/Pomodoro-clock/static/js/runtime-main.c65b8406.js"
+  }
+]);
